5ebea7c57a0e67affa63cbba4488fab50c8d9e90
pete gray <peteragray@gmail.com>
2026-04-10 15:02:28 +1000

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import AuroraHealth from "./AuroraHealth.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuroraHealth />
-  </React.StrictMode>
-);
